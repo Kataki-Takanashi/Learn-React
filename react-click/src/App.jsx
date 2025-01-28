54325ed66be6b488e6a878f027bf67e9979b7c86
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-28 12:02:25 -0500

    feat(click): Stop Clicking

--- react-click/src/App.jsx
+++ react-click/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Button from "./Button"
 
 function App() {
   
 
   return (
     <>
-      
+      <Button />
     </>
   )
 }
--- react-click/src/Button.jsx
+++ react-click/src/Button.jsx
@@ -1,10 +1,18 @@
 
 function Button() {
 
+    let count = 0;
+    const handleClick = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`You clicked me ${count} times`);
+        } else {
+            console.log(`${name} stop clicking me`);
+        }
+    }
 
     return (
-        <>
-        </>
+        <button onClick={() => {handleClick("Ali")}}>Click Me! 😃</button>
     );
 }
 
