550619b248a2e77c2576e1fc987e67f4b8b168dc
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-28 12:12:27 -0500

    feat(click): Double Click

--- react-click/src/Button.jsx
+++ react-click/src/Button.jsx
@@ -3,12 +3,19 @@ function Button() {
 
     
     const handleClick = (e) => {
-        e.target.textContent = "OUCH! 😱";
+        e.target.textContent = "Clicked! 😄";
+        setTimeout(() => {e.target.textContent = "Click Me! 😃";}, 1000);
+    }
+
+    const handleDoubleClick = (e) => {
+        e.target.textContent = "OUCH! 🤕";
         setTimeout(() => {e.target.textContent = "Click Me! 😃";}, 1000);
     }
 
     return (
-        <button onClick={(e) => {handleClick(e)}}>Click Me! 😃</button>
+        <button onClick={(e) => {handleClick(e)}} onDoubleClick={(e) => {handleDoubleClick(e)}}>
+            Click Me! 😃
+        </button>
     );
 }
 
