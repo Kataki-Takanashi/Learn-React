719b088f417ccb7939fd64b17eb3b94837f730cd
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-28 14:46:22 -0500

    feat(counter): Employed

--- react-counter/src/MyComponent.jsx
+++ react-counter/src/MyComponent.jsx
@@ -1,10 +1,33 @@
+import React, { useState } from "react";
+
 
 function MyComponent() {
 
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
+
+    const updateName = () => {
+        setName("Ali AbdurRaheem");
+    }
+
+    const incrementAge = () => {
+        setAge(age + 1);
+    }
+
+    const toggleEmployed = () => {
+        setIsEmployed(!isEmployed);
+    }
 
     return (
-        <>
-        </>
+        <div>
+            <p>Name: {name}</p>
+            <button onClick={updateName}>Set Name</button>
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>Increment Age</button>
+            <p>{name} is {isEmployed? "Employed" : "Unemployed"}</p>
+            <button onClick={toggleEmployed}>Toggle Employed</button>
+        </div>
     );
 }
 
