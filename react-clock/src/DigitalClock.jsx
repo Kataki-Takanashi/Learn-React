21b3ce28902dd6da4ca3838e51255b604305ca48
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-29 15:50:54 -0500

    feat(clock): Done

--- react-clock/src/DigitalClock.jsx
+++ react-clock/src/DigitalClock.jsx
@@ -3,10 +3,36 @@ import React, {useState, useEffect} from "react";
 
 function DigitalClock() {
 
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const formatTime = (time) => {
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridiem = hours >= 12? "PM" : "AM";
+        
+        hours = hours % 12 || 12;
+        
+        return `${hours}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+    }
+
+        
+    const padZero = (number) => number < 10 ? `0${number}` : number;
+
+        
+
     return (
         <div className="clock-container">
             <div className="clock">
-                <span>00:00:00</span> {/* Placeholder */}
+                <span>{formatTime(time)}</span>
             </div>
 
         </div>
