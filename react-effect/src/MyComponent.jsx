5b19de8bfe41c4bf3f18c3bcdc8f5ca85cdbd704
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-29 13:23:36 -0500

    feat(effect): Update Color on change

--- react-effect/src/MyComponent.jsx
+++ react-effect/src/MyComponent.jsx
@@ -3,13 +3,16 @@ import React, { useState, useEffect } from "react";
 function MyComponent() {
 
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState('green');
 
-    useEffect(() => {document.title = `Count: ${count}`, [count]});
+    useEffect(() => {document.title = `Count: ${count} ${color}`, [count, color]});
 
     return (
         <>
-            <p>Count: {count}</p>
+            <p style={{color: color}}>Count: {count}</p>
             <button onClick={() => setCount(c => c + 1)}>Increment</button>
+            <button onClick={() => setCount(c => c - 1)}>Subtract</button><br/>
+            <button onClick={() => setColor(c => c === 'green' ? 'red' : 'green')}>Change Color</button>
         </>
     );
 }
