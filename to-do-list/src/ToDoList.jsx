e95e822eb3c30255c4fa6f9443605316db28ded2
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-29 10:14:50 -0500

    feat(ToDo): Done HTML

--- to-do-list/src/ToDoList.jsx
+++ to-do-list/src/ToDoList.jsx
@@ -1,13 +1,49 @@
 import React, { useState } from "react";
 
 function ToDoList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTasks] = useState("");
 
+    function handleInputChange(e) {
+    }
 
+    function addTask() {
+    }
+
+    function deleteTask(index) {
+    }
+
+    function moveTaskUp(index) {
+    }
+
+    function moveTaskDown(index) {
+    }
 
     
     return (
-        <div>
-            
+        <div className="to-do-list">
+            <h1>To-Do-List</h1>
+
+            <div>
+            <input 
+                type="text" 
+                value={newTask} 
+                onChange={handleInputChange} 
+                placeholder="Enter a task..."
+            />
+                <button onClick={addTask} className="add-button">Add</button>
+            </div>
+
+            <ol>
+                {tasks.map((task, index) => (
+                    <li key={index}>
+                        <span className="text">{task}</span>
+                        <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+                        <button className="move-button" onClick={() => moveTaskUp(index)}>👆</button>
+                        <button className="move-button" onClick={() => moveTaskDown(index)}>👇</button>
+                    </li>
+                ))}
+            </ol>
         </div>
     );
 }
