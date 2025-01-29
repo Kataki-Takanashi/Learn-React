211f9c1587f3caa2fa087522a508fff351f94f5f
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-29 09:00:17 -0500

    feat(array): Done

--- react-array/src/MyComponent.jsx
+++ react-array/src/MyComponent.jsx
@@ -1,9 +1,56 @@
+import React, { useState } from "react";
 
 function MyComponent() {
 
+    const [cars , setCars] = useState([]);
+    const [carYear, setCarYear] = useState(new Date().getFullYear());
+    const [carMake, setCarMake] = useState("");
+    const [carModel, setCarModel] = useState("");
+
+    function handleAddCar() {
+         if (carYear === "" || carMake === "" || carModel === "") return;
+
+        const newCar = {
+            year: carYear,
+            make: carMake,
+            model: carModel,
+        };
+        setCars(c => [...c, newCar]);
+
+        // Clear Input Fields
+        setCarYear(new Date().getFullYear());
+        setCarMake("");
+        setCarModel("");
+    }
+    function handleRemoveCar(index) {
+        setCars(c => c.filter((_, i) => i !== index)); // The !== is a strict not equal operator
+    }
+    function handleYearChange(e) {
+        setCarYear(e.target.value);
+    }
+    function handleMakeChange(e) {
+        setCarMake(e.target.value);
+    }
+    function handleModelChange(e) {
+        setCarModel(e.target.value);
+    }
+
+
     return (
         <div>
-            
+            <h2>List of Car Objects</h2>
+            <ul>
+                {cars.map((car, index) => (
+                    <li key={index} onClick={() => handleRemoveCar(index)}>
+                        {car.year} {car.make} {car.model}
+                    </li>
+                ))}
+            </ul>
+
+            <input type="number" value={carYear} onChange={handleYearChange} placeholder="Enter car year"/>
+            <input type="text" value={carMake} onChange={handleMakeChange} placeholder="Enter car make"/>
+            <input type="text" value={carModel} onChange={handleModelChange} placeholder="Enter car model"/>
+            <button onClick={handleAddCar}>Add Car</button>
         </div>
     );
 }
