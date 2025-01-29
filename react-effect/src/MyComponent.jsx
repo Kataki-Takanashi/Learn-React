47f3fd6ce8fa3eaa3a93f3060da6294bccb3a03f
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-29 11:20:16 -0500

    feat(effect): Add Counter

---
+++ react-effect/src/MyComponent.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+function MyComponent() {
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(c => c + 1)}>Increment</button>
+        </>
+    );
+}
+
+export default MyComponent;
